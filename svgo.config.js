4ca9f6f7051f6ae19e3425bf6892ebd63b14bd20
Luis Mendes <machadoluismendes@gmail.com>
2021-11-02 20:13:52 -0300

    github merge branch
    json comments deleted

--- svgo.config.js
+++ svgo.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e4885f6ece9e55e317e8734c5d26077948d0c4edd48b97401beec52e2626545
-size 1090
+oid sha256:4e3172ea3ceda75e116833ecd41b1ee2f0cdb97f75f044929858476cdd1bda54
+size 690

f692dcfd6c54fe20f99fff355b51c203ce2761ce
Luis Mendes <machadoluismendes@gmail.com>
2021-11-10 16:15:57 -0300

    eslint increment
    karma-typescript docker reducer config

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45e744270a777b7f491f20977ee9e64e2c5176b10b425a7d71d81e3dfc72c2e6
-size 814
+oid sha256:6fee7fef51d5ba5f4098c34887ae16a455fff9096592b71e8b84e71c9242937e
+size 859

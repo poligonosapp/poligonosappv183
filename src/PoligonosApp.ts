ca6f6259dafb719a79267df555d1ce71a0b3376d
Luis Mendes <machadoluismendes@gmail.com>
2021-11-13 21:26:14 -0300

    eslint increment
    Polygon mock increment

--- src/PoligonosApp.ts
+++ src/PoligonosApp.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dda642f99b5c52862e11cf536f54696b63cc7061dab31596a33510445a861aff
-size 5966
+oid sha256:5f913a1d68cbf25dee18ad96e0ac7b23f4a3510429cbe1da239680d1f03a7391
+size 6428

885a325ff184d77ca116d4c1dfbc02a43e050956
Luis Mendes <machadoluismendes@gmail.com>
2021-11-15 20:04:55 -0300

    eslint increment
    jest mock fail

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffd444f2f11587076f1837b97a05d11495835474bbb0b5332b94bf9bce8efd57
-size 6681
+oid sha256:ae0c79e0ffa4d1c2c08cf7462b2c16f28e69a31a567467a16102518b97704023
+size 6801

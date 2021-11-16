db991d1fabda3cc9acb013f660bebae76437620e
Luis Mendes <machadoluismendes@gmail.com>
2021-11-16 00:59:47 -0300

    eslint increment

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae0c79e0ffa4d1c2c08cf7462b2c16f28e69a31a567467a16102518b97704023
-size 6801
+oid sha256:3a6e6067d6fe7da877ef3e961b4fd292a3dbc97a8de7706d09bafbb2d19c37cb
+size 7049

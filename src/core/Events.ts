0b27981ba1fcf3b8d130e6949f62688eb805a06d
Luis Mendes <machadoluismendes@gmail.com>
2021-11-18 16:35:30 -0300

    eslint increment
    React.CSSProperties

--- src/core/Events.ts
+++ src/core/Events.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9f8151b523a58330e0ad02ba25b1a4f6aeb278ce3a8d911dd072f53ca72b330
-size 11922
+oid sha256:1c36fdf9a17cf6e92348f964ee5d9ac579b1418ff3bd96808d083227e0998c90
+size 12507

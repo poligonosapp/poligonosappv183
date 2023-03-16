2ec3659287adae638d19ad123fdc426dbec548f8
luismendes070 <machadoluismendes@gmail.com>
2023-03-16 11:57:26 -0300

    increment any data type

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d23ac8e3adbb119800ce515664373fb17f6aea4a5fa3b6e95949f270b5a5cceb
-size 11999
+oid sha256:9cab086c26708838b3ce7411aa1be72511f39a8dde0f25bd6e6908c67f8c76d4
+size 12027
